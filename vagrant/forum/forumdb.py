d69dd49ff5b8ada045d1fc2b2afef6015b489cd9
Mighty Itauma <amightyo@gmail.com>
2015-02-10 10:35:22 -0500

    update forumdb

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,13 @@
 #
 # Database access functions for the web forum.
 # 
-
+import psycopg2
 import time
 
 ## Database connection
-DB = []
+#DB = []
+
+
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,9 +18,19 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    #posts.sort(key=lambda row: row['time'], reverse=True)
+    #UpdatePost()
+    DeleteCheese()
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    QUERY = "select time, content from posts order by time desc"
+    c.execute(QUERY)
+    posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+    #posts = c.fetchall()	
+    DB.close()
     return posts
+
+#'); delete from posts; --
 
 ## Add a post to the database.
 def AddPost(content):
@@ -27,5 +39,31 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    #QUERY = "insert into posts(content) values(%s)",(content,)
+    c.execute("insert into posts(content) values(%s)",(content,))
+    DB.commit()
+    DB.close()
+    #t = time.strftime('%c', time.localtime())
+    #DB.append((t, content))
+
+#Update post in the database
+def UpdatePost():
+    '''Update the content of the databse
+    '''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("UPDATE posts SET content='Cheese' where content like '%spam%'")
+    DB.commit()
+    DB.close()
+
+#Delete Cheese from from posts table
+def DeleteCheese():
+    '''Delete Cheese posts
+	'''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.execute("DELETE from posts WHERE content='Cheese'")
+    DB.commit()
+    DB.close()
