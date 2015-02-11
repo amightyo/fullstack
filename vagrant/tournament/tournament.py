550a38990018a0af5cb4068abf591670e8e0953b
Mighty Itauma <amightyo@gmail.com>
2015-02-10 19:00:51 -0500

    create schema and queries

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -13,14 +13,31 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE from matchresults")
+    DB.commit()
+    DB.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    DB = connect()
+    c = DB.cursor()
+    c.execute("DELETE from players")
+    DB.commit()
+    DB.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    DB = connect()
+    c = DB.cursor()
+    QUERY = "select count(*) as NumOfRegPlayers from players"
+    c.execute(QUERY)
+    NumOfRegPlayers = c.fetchall()[0][0]
+    DB.close()
+    return NumOfRegPlayers
 
 
 def registerPlayer(name):
@@ -32,6 +49,11 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    DB = connect()
+    c = DB.cursor()
+    c.execute("insert into players(name) values(%s)",(name,))
+    DB.commit()
+    DB.close()
 
 
 def playerStandings():
@@ -47,6 +69,22 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    DB = connect()
+    c = DB.cursor()
+    #QUERY = "select id, name, wins, matches from players, matchresults where players.id = matchresults.id  order by time desc"
+    QUERY = '''
+    select players.id, name, matchresults.wins as wins, (matchresults.wins + matchresults.loss) as matches
+    from players, matchresults 
+    where players.id = matchresults.id  
+    order by wins desc;
+    '''
+    c.execute(QUERY)
+    playerstanding = []
+    for row in c.fetchall():
+        playerstanding.append((str(row[0]), str(row[1]), str(row[2]), str(row[3])))
+    #playerstanding = ({'id': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
+    DB.close()
+    return posts
 
 
 def reportMatch(winner, loser):
@@ -56,6 +94,22 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    DB = connect()
+    c = DB.cursor()
+    #TODO
+    #If no update then insert
+    
+    #Update if already in table
+    c.execute("UPDATE matchresults SET wins=matchresults.wins + 1 where matchresults.id = %d",(winner,))
+    c.execute("UPDATE matchresults SET loss=matchresults.loss + 1 where matchresults.id = %d",(loser,))
+    #c.execute("insert into matchresults(winner, loser) values(%d,%d)",(name, loser,))
+    #c.execute("insert into matchresults(id, score) values(%d, 'winner')",(winner,))
+    #c.execute("insert into matchresults(id, score) values(%d, 'loser')",(loser,))
+    #Else Insert the new outcome
+    c.execute("insert into matchresults(id, wins, loss) values(%d, 1, 0)",(winner,))
+    c.execute("insert into matchresults(id, wins, loss) values(%d, 0, 1)",(loser,))
+    DB.commit()
+    DB.close()
  
  
 def swissPairings():
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,25 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+CREATE TABLE players(
+id serial primary key,
+name text);
+
+-- CREATE TABLE matchresults(
+-- winner integer,
+-- loser integer);
+
+--CREATE TABLE matchresults(
+--id integer,
+--score text);
+
+CREATE TABLE matchresults(
+id integer,
+wins integer,
+loss integer);
+
+
+
+--select players.id, name, matchresults.wins as wins, (matchresults.wins + matchresults.loss) as matches from players, matchresults where players.id = matchresults.id order by wins desc;
 
 
